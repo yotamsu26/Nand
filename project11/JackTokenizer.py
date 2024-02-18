305acf599243857b37f9a8cc279f3039fd348e46
Yotam Suliman <92324082+yotamsu26@users.noreply.github.com>
2024-02-18 17:28:57 +0200

    Add files via upload

---
+++ project11/JackTokenizer.py
@@ -0,0 +1,251 @@
+"""
+This file is part of nand2tetris, as taught in The Hebrew University, and
+was written by Aviv Yaish. It is an extension to the specifications given
+[here](https://www.nand2tetris.org) (Shimon Schocken and Noam Nisan, 2017),
+as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0
+Unported [License](https://creativecommons.org/licenses/by-nc-sa/3.0/).
+"""
+import typing
+import re
+
+# constants
+
+KEYWORD = "keyword"
+END_COMMENT = "END_COMMENT"
+START_COMMENT = "START_COMMENT"
+COMMENT = "COMMENT"
+SYMBOL = "symbol"
+IDENTIFIER = "identifier"
+INT_CONST = "integerConstant"
+STRING_CONST = "stringConstant"
+
+class JackTokenizer:
+    """Removes all comments from the input stream and breaks it
+    into Jack language tokens, as specified by the Jack grammar.
+    
+    # Jack Language Grammar
+
+    A Jack file is a stream of characters. If the file represents a
+    valid program, it can be tokenized into a stream of valid tokens. The
+    tokens may be separated by an arbitrary number of whitespace characters, 
+    and comments, which are ignored. There are three possible comment formats: 
+    /* comment until closing */ , /** API comment until closing */ , and 
+    // comment until the line's end.
+
+    - 'xxx': quotes are used for tokens that appear verbatim ('terminals').
+    - xxx: regular typeface is used for names of language constructs 
+           ('non-terminals').
+    - (): parentheses are used for grouping of language constructs.
+    - x | y: indicates that either x or y can appear.
+    - x?: indicates that x appears 0 or 1 times.
+    - x*: indicates that x appears 0 or more times.
+
+    ## Lexical Elements
+
+    The Jack language includes five types of terminal elements (tokens).
+
+    - keyword: 'class' | 'constructor' | 'function' | 'method' | 'field' | 
+               'static' | 'var' | 'int' | 'char' | 'boolean' | 'void' | 'true' |
+               'false' | 'null' | 'this' | 'let' | 'do' | 'if' | 'else' | 
+               'while' | 'return'
+    - symbol: '{' | '}' | '(' | ')' | '[' | ']' | '.' | ',' | ';' | '+' | 
+              '-' | '*' | '/' | '&' | '|' | '<' | '>' | '=' | '~' | '^' | '#'
+    - integerConstant: A decimal number in the range 0-32767.
+    - StringConstant: '"' A sequence of Unicode characters not including 
+                      double quote or newline '"'
+    - identifier: A sequence of letters, digits, and underscore ('_') not 
+                  starting with a digit. You can assume keywords cannot be
+                  identifiers, so 'self' cannot be an identifier, etc'.
+
+    ## Program Structure
+
+    A Jack program is a collection of classes, each appearing in a separate 
+    file. A compilation unit is a single class. A class is a sequence of tokens 
+    structured according to the following context free syntax:
+    
+    - class: 'class' className '{' classVarDec* subroutineDec* '}'
+    - classVarDec: ('static' | 'field') type varName (',' varName)* ';'
+    - type: 'int' | 'char' | 'boolean' | className
+    - subroutineDec: ('constructor' | 'function' | 'method') ('void' | type) 
+    - subroutineName '(' parameterList ')' subroutineBody
+    - parameterList: ((type varName) (',' type varName)*)?
+    - subroutineBody: '{' varDec* statements '}'
+    - varDec: 'var' type varName (',' varName)* ';'
+    - className: identifier
+    - subroutineName: identifier
+    - varName: identifier
+
+    ## Statements
+
+    - statements: statement*
+    - statement: letStatement | ifStatement | whileStatement | doStatement | 
+                 returnStatement
+    - letStatement: 'let' varName ('[' expression ']')? '=' expression ';'
+    - ifStatement: 'if' '(' expression ')' '{' statements '}' ('else' '{' 
+                   statements '}')?
+    - whileStatement: 'while' '(' 'expression' ')' '{' statements '}'
+    - doStatement: 'do' subroutineCall ';'
+    - returnStatement: 'return' expression? ';'
+
+    ## Expressions
+    
+    - expression: term (op term)*
+    - term: integerConstant | stringConstant | keywordConstant | varName | 
+            varName '['expression']' | subroutineCall | '(' expression ')' | 
+            unaryOp term
+    - subroutineCall: subroutineName '(' expressionList ')' | (className | 
+                      varName) '.' subroutineName '(' expressionList ')'
+    - expressionList: (expression (',' expression)* )?
+    - op: '+' | '-' | '*' | '/' | '&' | '|' | '<' | '>' | '='
+    - unaryOp: '-' | '~' | '^' | '#'
+    - keywordConstant: 'true' | 'false' | 'null' | 'this'
+    
+    Note that ^, # correspond to shiftleft and shiftright, respectively.
+    """
+    #regex
+    STRING_REGEX = '"([^"]*)"'
+    IDENTIFIER_REGEX = "[a-zA-Z_][\w_]*"
+    NUMBER_REGEX = '\d+'
+    SYMBOL_REGEX = '[\/[|\]|\{|\}|\(|\)|\.\,;\+\-*&|<>=~\^#]'
+    KEYWORD_REGEX = 'class|constructor|function|method|' \
+                    'field|static|var|int|char|' \
+                    'boolean|void|true|false|' \
+                    'null|this|let|do|if|else|while|return'
+    ONELINE_COMMENT_REGEX = r"\/\/.*$"
+    API_COMMENT_START = r"\/\*((?:(?!\*\/).)*)"
+    MULTILINE_COMMENT_END = r"\*\/"
+
+    COMBINED_REGEX = fr"(?P<{COMMENT}>({ONELINE_COMMENT_REGEX}))|"\
+                     fr"(?P<{START_COMMENT}>({API_COMMENT_START}))|"\
+                     fr"(?P<{END_COMMENT}>({MULTILINE_COMMENT_END}))|" \
+                     fr"(?P<{KEYWORD}>\b({KEYWORD_REGEX})\b)|"\
+                     fr"(?P<{SYMBOL}>({SYMBOL_REGEX}))|" \
+                     fr"(?P<{STRING_CONST}>({STRING_REGEX}))|"\
+                     fr"(?P<{IDENTIFIER}>({IDENTIFIER_REGEX}))|" \
+                     fr"(?P<{INT_CONST}>({NUMBER_REGEX}))"
+
+
+    def __init__(self, input_stream: typing.TextIO) -> None:
+        """Opens the input stream and gets ready to tokenize it.
+
+        Args:
+            input_stream (typing.TextIO): input stream.
+        """
+        # Your code goes here!
+        # A good place to start is to read all the lines of the input:
+        self._input_lines = input_stream.read().splitlines()
+        self._cur_token = ""
+        self._cur_type = None
+        self._next_token = ""
+        self._next_type = None
+        self._token_gen = self._token_generator() #create the generator
+        self._in_comment = False
+        self.advance()
+        self.advance()
+
+    def has_more_tokens(self) -> bool:
+        """Do we have more tokens in the input?
+
+        Returns:
+            bool: True if there are more tokens, False otherwise.
+        """
+        return self._next_token is not None
+
+    def _token_generator(self) -> typing.Tuple[str, str]:
+        """
+        this is generator that each time yield the tuple(token type, token value)
+        Returns: the current token
+        """
+        for line in self._input_lines:
+            matches = re.finditer(JackTokenizer.COMBINED_REGEX, line)
+            for match in matches:
+                yield match.lastgroup, match.group(match.lastgroup)
+        while True:
+            yield None, None
+
+    def advance(self) -> None:
+        """Gets the next token from the input and makes it the current token. 
+        This method should be called if has_more_tokens() is true. 
+        Initially there is no current token.
+        """
+        self._cur_type, self._cur_token = self._next_type, self._next_token
+        self._next_type, self._next_token = next(self._token_gen)
+        #comment handle
+        if self._cur_type == START_COMMENT:
+            self._in_comment = True
+        elif self._cur_type == END_COMMENT:
+            self._in_comment = False
+            self.advance()
+        while self._in_comment or self._cur_type == COMMENT:
+            self.advance()
+        #remove " from strings
+        if self._cur_type == STRING_CONST:
+            self._cur_token = self._cur_token[1:-1]
+
+
+    def token_type(self) -> str:
+        """
+        Returns:
+            str: the type of the current token, can be
+            "KEYWORD", "SYMBOL", "IDENTIFIER", "INT_CONST", "STRING_CONST"
+        """
+        return self._cur_type
+
+
+
+    def keyword(self) -> str:
+        """
+        Returns:
+            str: the keyword which is the current token.
+            Should be called only when token_type() is "KEYWORD".
+            Can return "CLASS", "METHOD", "FUNCTION", "CONSTRUCTOR", "INT", 
+            "BOOLEAN", "CHAR", "VOID", "VAR", "STATIC", "FIELD", "LET", "DO", 
+            "IF", "ELSE", "WHILE", "RETURN", "TRUE", "FALSE", "NULL", "THIS"
+        """
+        return self._cur_token
+
+    def symbol(self) -> str:
+        """
+        Returns:
+            str: the character which is the current token.
+            Should be called only when token_type() is "SYMBOL".
+            Recall that symbol was defined in the grammar like so:
+            symbol: '{' | '}' | '(' | ')' | '[' | ']' | '.' | ',' | ';' | '+' | 
+              '-' | '*' | '/' | '&' | '|' | '<' | '>' | '=' | '~' | '^' | '#'
+        """
+        return self._cur_token
+
+
+    def identifier(self) -> str:
+        """
+        Returns:
+            str: the identifier which is the current token.
+            Should be called only when token_type() is "IDENTIFIER".
+            Recall that identifiers were defined in the grammar like so:
+            identifier: A sequence of letters, digits, and underscore ('_') not 
+                  starting with a digit. You can assume keywords cannot be
+                  identifiers, so 'self' cannot be an identifier, etc'.
+        """
+        return self._cur_token
+
+
+    def int_val(self) -> int:
+        """
+        Returns:
+            str: the integer value of the current token.
+            Should be called only when token_type() is "INT_CONST".
+            Recall that integerConstant was defined in the grammar like so:
+            integerConstant: A decimal number in the range 0-32767.
+        """
+        return int(self._cur_token)
+
+    def string_val(self) -> str:
+        """
+        Returns:
+            str: the string value of the current token, without the double 
+            quotes. Should be called only when token_type() is "STRING_CONST".
+            Recall that StringConstant was defined in the grammar like so:
+            StringConstant: '"' A sequence of Unicode characters not including 
+                      double quote or newline '"'
+        """
+        return self._cur_token
